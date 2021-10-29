fe34d20148041f5ea263679bdda576b1553e9d24
OLATUNDE <olatundequadril@gmail.com>
2021-10-29 09:40:31 -0400

    clear

--- assets/script.js
+++ assets/script.js
@@ -15,10 +15,11 @@ let sugar = 0   // Sugar Sprinkle
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-let totalGingerbread = 0;
-let totalChocolateChip = 0;
-let totalSugarSprinkle = 0;
-let totalQuantity = 0;
+
+let totalQuantity1 = 0;
+let totalQuantity2 = 0;
+let totalQuantity3 = 0;
+
 
 let quantitygb = document.querySelector('#qty-gb');
 let overallTotal = document.querySelector('#qty-total');
@@ -26,25 +27,24 @@ let overallTotal = document.querySelector('#qty-total');
 
 // console.log(totalQuantity);
 
-let GingerbreadMinus = document.querySelector('#add-gb');
+let GingerbreadPlus = document.querySelector('#add-gb');
     // console.log(GingerbreadMinus)
-    GingerbreadMinus.addEventListener('click', function() {
+    GingerbreadPlus.addEventListener('click', function() {
     //console.log('Gingerbread - button was clicked!')
-    totalQuantity = totalQuantity + 1;
-    console.log(totalQuantity)
-    quantitygb.textContent = totalQuantity;
-    overallTotal.textContent = totalQuantity ;
+    totalQuantity1 = totalQuantity1 + 1;
+    console.log(totalQuantity1)
+    quantitygb.textContent = totalQuantity1;
+    overallTotal.textContent = totalQuantity1 + totalQuantity2 +totalQuantity3;
 
 });
 
-let GingerbreadPlus = document.querySelector('#minus-gb');
-        GingerbreadPlus.addEventListener('click', function() {
+let GingerbreadMinus = document.querySelector('#minus-gb');
+        GingerbreadMinus.addEventListener('click', function() {
         // console.log('Gingerbread + button was clicked!') 
-        if (totalQuantity > 0 ){
-            totalQuantity= totalQuantity - 1};
-            quantitygb.textContent = totalQuantity; 
-            overallTotal.textContent = totalQuantity;
-
+        if (totalQuantity1 > 0 ){
+            totalQuantity1= totalQuantity1 - 1};
+            quantitygb.textContent = totalQuantity1; 
+            overallTotal.textContent = totalQuantity1 + totalQuantity2 +totalQuantity3;
 });
 
 let quantitycc = document.querySelector('#qty-cc');
@@ -53,19 +53,19 @@ let quantitycc = document.querySelector('#qty-cc');
 let ChocolateChipPlus= document.querySelector('#add-cc');
     ChocolateChipPlus.addEventListener('click', function() {
         // console.log('chocolate chip + button was clicked!') 
-        totalQuantity = totalQuantity + 1;
-        quantitycc.textContent = totalQuantity;
-        overallTotal.textContent = totalQuantity;
+        totalQuantity2 = totalQuantity2 + 1;
+        quantitycc.textContent = totalQuantity2;
+        overallTotal.textContent = totalQuantity1 + totalQuantity2 +totalQuantity3;
     });
 
     let ChocolateChipMinus = document.querySelector('#minus-cc');
    
     ChocolateChipMinus.addEventListener('click', function() {
     // console.log('chocolate chip - button was clicked!')
-    if (totalQuantity > 0 ){
-        totalQuantity= totalQuantity - 1};
-        quantitycc.textContent = totalQuantity;
-        overallTotal.textContent = totalQuantity;
+    if (totalQuantity2 > 0 ){
+        totalQuantity2= totalQuantity - 1};
+        quantitycc.textContent = totalQuantity2;
+        overallTotal.textContent = totalQuantity1 + totalQuantity2 +totalQuantity3;
 })
 
 let quantityss = document.querySelector('#qty-sugar');
@@ -73,18 +73,18 @@ let quantityss = document.querySelector('#qty-sugar');
     let SugarSprinklePlus = document.querySelector('#add-sugar');
     SugarSprinklePlus.addEventListener('click', function() {
     // console.log('Gingerbread + button was clicked!') 
-    totalQuantity = totalQuantity + 1;
-    quantityss.textContent = totalQuantity;
-    overallTotal.textContent = totalQuantity;
+    totalQuantity3 = totalQuantity3 + 1;
+    quantityss.textContent = totalQuantity3;
+    overallTotal.textContent = totalQuantity1 + totalQuantity2 +totalQuantity3;
 })
 let SugarSprinkleMinus = document.querySelector('#minus-sugar');
         SugarSprinkleMinus.addEventListener('click', function() {
             // console.log('Gingerbread - button was clicked!')
             if (totalQuantity > 0 ){
-                totalQuantity= totalQuantity - 1};
-                quantityss.textContent = totalQuantity;
-                overallTotal.textContent = totalQuantity;
+                totalQuantity3= totalQuantity3 - 1};
+                quantityss.textContent = totalQuantity3;
+                overallTotal.textContent = totalQuantity1 + totalQuantity2 +totalQuantity3;
         })
         
-
+let
     
